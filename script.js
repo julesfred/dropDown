87dff37cb713f6835340d01b21e0906fd04177ab
julesfred <jules.tf4@yahoo.com>
2017-11-12 18:48:04 -0500

    css/jquery

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ $(document).ready(function() {
 $(".oneCircle").on("click", function(event) {
     console.log("clicked");
     $(".oneDrop").slideToggle(1000);
+
+});
+
+$(".twoCircle").on("click", function(event) {
+    console.log("clicked");
+    $(".twoDrop").slideToggle(1000);
+
+});
+
+$(".threeCircle").on("click", function(event) {
+    console.log("clicked");
+    $(".threeDrop").slideToggle(1000);
+
+});
+
+$(".fourCircle").on("click", function(event) {
+    console.log("clicked");
+    $(".fourDrop").slideToggle(1000);
+
+});
+
+$(".fiveCircle").on("click", function(event) {
+    console.log("clicked");
+    $(".fiveDrop").slideToggle(1000);
+
 });
 
 });
